e1ef6a15f576eb4c2917555f02769f22f3e99406
blusky10 <blusky0910@gmail.com>
2019-05-14 23:28:20 +0900

    Update: 설정 추가

--- polpid-eurekaserver/src/main/java/com/polpid/polpideurekaserver/PolpidEurekaserverApplication.java
+++ polpid-eurekaserver/src/main/java/com/polpid/polpideurekaserver/PolpidEurekaserverApplication.java
@@ -2,8 +2,10 @@ package com.polpid.polpideurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PolpidEurekaserverApplication {
 
 	public static void main(String[] args) {
