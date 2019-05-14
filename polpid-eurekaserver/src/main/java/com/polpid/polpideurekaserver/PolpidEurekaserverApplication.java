c505234bd668b4d7fc6605873eccead97011d4d0
blusky10 <blusky0910@gmail.com>
2019-05-14 22:54:47 +0900

    Update: eureakserver 추가

---
+++ polpid-eurekaserver/src/main/java/com/polpid/polpideurekaserver/PolpidEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.polpid.polpideurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolpidEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolpidEurekaserverApplication.class, args);
+	}
+
+}
