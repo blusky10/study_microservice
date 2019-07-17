dc014d7258267fb961b3ff80d5d4c51b7c07d971
blusky10 <blusky0910@gmail.com>
2019-07-17 23:44:07 +0900

    create: user-service

---
+++ polpid-user-service/src/main/java/com/polpid/polpiduserservice/PolpidUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polpid.polpiduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolpidUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolpidUserServiceApplication.class, args);
+	}
+
+}
