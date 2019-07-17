91258756fa6c4a04a4e70809d6081d7cad9dffe6
blusky10 <blusky0910@gmail.com>
2019-07-17 23:36:55 +0900

    creatae: polpid-config-server

---
+++ polpid-config-server/src/main/java/com/polpid/polpidconfigserver/PolpidConfigServerApplication.java
@@ -0,0 +1,27 @@
+package com.polpid.polpidconfigserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@Slf4j
+public class PolpidConfigServerApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolpidConfigServerApplication.class, args);
+	}
+
+	@Value("${spring.application.name}")
+	String application;
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Application : " + application);
+	}
+
+}
