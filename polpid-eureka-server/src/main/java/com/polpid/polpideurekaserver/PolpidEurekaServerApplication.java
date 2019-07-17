7a5add9bdcf838923e39634dbb77bc138f520cce
blusky10 <blusky0910@gmail.com>
2019-07-17 23:49:49 +0900

    create: eureka-server

---
+++ polpid-eureka-server/src/main/java/com/polpid/polpideurekaserver/PolpidEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.polpid.polpideurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolpidEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolpidEurekaServerApplication.class, args);
+	}
+
+}
