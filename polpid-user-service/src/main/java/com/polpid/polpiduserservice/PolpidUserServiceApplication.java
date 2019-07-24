3c261ac99504984eda09a41473196c1bfea31e63
blusky10 <blusky0910@gmail.com>
2019-07-24 23:48:34 +0900

    설정 추가 및 변경

--- polpid-user-service/src/main/java/com/polpid/polpiduserservice/PolpidUserServiceApplication.java
+++ polpid-user-service/src/main/java/com/polpid/polpiduserservice/PolpidUserServiceApplication.java
@@ -1,12 +1,24 @@
 package com.polpid.polpiduserservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PolpidUserServiceApplication {
+@Slf4j
+public class PolpidUserServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolpidUserServiceApplication.class, args);
+	}
+
+	@Value("${spring.application.name}")
+	String application;
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Application : " + application);
 	}
 }
